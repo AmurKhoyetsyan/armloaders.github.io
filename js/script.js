72add5eac72931b27ab57b13f8abc02d59387fa3
AmurKhoyetsyan <javanewss@gmail.com>
2020-10-26 10:52:37 +0400

    add circule progress bar

--- js/script.js
+++ js/script.js
@@ -142,3 +142,29 @@
 
     init(glob.querySelector('.clock-parent'));
 })(document);
+
+;(function(){
+    var circle = document.getElementsByClassName('circle-progress');
+
+    if(circle === null) {
+        return false;
+    }
+
+    var options = {
+        type: 'circle',
+        text: true,
+        fontColor: '#333333',
+        fontSize: 32,
+        fontWeight: 900,
+        fillParent: '#FFFFFF',
+        fillChild: 'transparent',
+        interval: 1000,
+        animated: true,
+        strokeWidthParent: 10,
+        strokeWidthChild: 10,
+        progressColor: '#00AAFF',
+        progressParentColor: '#CECECE'
+    };
+    
+    new Progress(circle, options).inPercent();
+})();
