8f01f1e93c350e97a28d77df86e50d1fbae08830
AmurKhoyetsyan <javanewss@gmail.com>
2021-03-22 18:34:40 +0400

    add effect matrica

--- js/script.js
+++ js/script.js
@@ -284,3 +284,45 @@
 
     start();
 })(document);
+
+;(function(){
+    let canvas = document.querySelector(".matrix-canvas-1");
+
+    if(!canvas) {
+        return false;
+    }
+
+    let ctx = canvas.getContext("2d");
+    let width = canvas.width;
+    let height = canvas.height;
+    let str = "A+jk js:2 @dfs 17 tr YY ufds M5r P87 #18 $!& ^dfs $Ew er JH # $ * . (! ;) ,: :";
+    let matrix = str.split("");
+    let font = 5;
+    let col = width / font;
+    let arr = [];
+
+    for(let i = 0; i < col; i++) {
+        arr[i] = 1;
+    }
+
+    const draw = () => {
+        ctx.fillStyle = "rgba(0,0,0,0.05)";
+        ctx.fillRect(0, 0, width, height);
+        ctx.fillStyle = "#00FF00";
+        ctx.font = `${font}px system-ui`;
+
+        for(let i = 0; i < arr.length; i++) {
+            let txt = matrix[Math.floor(Math.random() * matrix.length)];
+            ctx.fillText(txt, i * font, arr[i] * font);
+
+            if(arr[i] * font > height && Math.random() > 0.975) {
+                arr[i] = 0;
+            }
+            arr[i]++;
+        }
+    }
+
+    setInterval(draw, 20);
+
+    window.addEventListener("resize", () => location.reload());
+})();
